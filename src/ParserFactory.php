9cbf62c0de3ea16617b404b161c95a33332dfa85
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-02-08 04:26:03 +0400

    Add ParserFactory.php

---
+++ src/ParserFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Hexlet\Code;
+
+class ParserFactory
+{
+    const ALLOWED_EXTENSIONS = [
+        'json' => \Hexlet\Code\parsers\JsonParser::class,
+    ];
+
+    public static function build(string $path): ParserInterface
+    {
+        $extension = pathinfo($path, PATHINFO_EXTENSION);
+        if (!isset(self::ALLOWED_EXTENSIONS[$extension])) {
+            throw new \Exception('Unsupported file format');
+        }
+        $parserClass = self::ALLOWED_EXTENSIONS[$extension];
+        $parser = new $parserClass();
+        return $parser;
+    }
+}
