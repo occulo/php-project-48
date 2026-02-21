efb9f24ad509c611c2a408ee2d12610842219033
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-02-22 00:53:39 +0400

    test(formatter): add tests for stylish formatter

---
+++ tests/FormatterTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use Hexlet\Code\ParserFactory;
+use Hexlet\Code\FormatterFactory;
+use Hexlet\Code\Differ;
+
+class FormatterTest extends TestCase
+{
+    public function testStylishFormatting(): void
+    {
+        $firstPath = __DIR__ . '/fixtures/file1.json';
+        $secondPath = __DIR__ . '/fixtures/file2.json';
+        $firstFile = ParserFactory::build($firstPath)->parse($firstPath);
+        $secondFile = ParserFactory::build($secondPath)->parse($secondPath);
+        $differ = new Differ();
+        $diff = $differ->genDiff($firstFile, $secondFile);
+        $actual = FormatterFactory::build('stylish')->format($diff);
+        $expected = file_get_contents(__DIR__ . '/fixtures/expected_stylish.txt');
+        $this->assertSame(trim($expected), trim($actual));
+    }
+}
