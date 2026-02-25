5d02b97f15998ab9653ce31337a5654e3fb4571b
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-02-25 18:59:20 +0400

    test(parser): add tests for yaml parsing

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -9,7 +9,30 @@ class ParserTest extends TestCase
 {
     public function testJsonParsing(): void
     {
-        $filePath = __DIR__ . '/fixtures/file1.json';
+        $filePath = __DIR__ . '/fixtures/json/file1.json';
+        $actual = ParserFactory::build($filePath)->parse($filePath);
+        $expected = [
+            'host' => 'hexlet.io',
+            'timeout' => 50,
+            'proxy' => '123.234.53.22',
+            'follow' => false,
+            'ports' => [80, 443, 8080],
+            'settings' => [
+                'mode' => 'development',
+                'retry' => 3,
+                'features' => [
+                    'auth' => true,
+                    'cache' => false,
+                    'beta' => true
+                ]
+            ]
+        ];
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testYamlParsing(): void
+    {
+        $filePath = __DIR__ . '/fixtures/yaml/file1.yaml';
         $actual = ParserFactory::build($filePath)->parse($filePath);
         $expected = [
             'host' => 'hexlet.io',
