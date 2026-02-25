77502c3220afe6cbf3e306940ec8bfbd866de17b
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-02-25 19:16:12 +0400

    test(parser): update expected values

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -12,20 +12,14 @@ class ParserTest extends TestCase
         $filePath = __DIR__ . '/fixtures/json/file1.json';
         $actual = ParserFactory::build($filePath)->parse($filePath);
         $expected = [
-            'host' => 'hexlet.io',
-            'timeout' => 50,
-            'proxy' => '123.234.53.22',
-            'follow' => false,
-            'ports' => [80, 443, 8080],
-            'settings' => [
-                'mode' => 'development',
-                'retry' => 3,
-                'features' => [
-                    'auth' => true,
-                    'cache' => false,
-                    'beta' => true
-                ]
-            ]
+            'common' => [
+                'setting1' => 'Value 1',
+                'setting2' => 200,
+                'setting3' => true,
+                'setting6' => ['key' => 'value', 'doge' => ['wow' => '']],
+            ],
+            'group1' => ['baz' => 'bas', 'foo' => 'bar', 'nest' => ['key' => 'value']],
+            'group2' => ['abc' => 12345, 'deep' => ['id' => 45]],
         ];
         $this->assertEquals($expected, $actual);
     }
@@ -35,20 +29,14 @@ class ParserTest extends TestCase
         $filePath = __DIR__ . '/fixtures/yaml/file1.yaml';
         $actual = ParserFactory::build($filePath)->parse($filePath);
         $expected = [
-            'host' => 'hexlet.io',
-            'timeout' => 50,
-            'proxy' => '123.234.53.22',
-            'follow' => false,
-            'ports' => [80, 443, 8080],
-            'settings' => [
-                'mode' => 'development',
-                'retry' => 3,
-                'features' => [
-                    'auth' => true,
-                    'cache' => false,
-                    'beta' => true
-                ]
-            ]
+            'common' => [
+                'setting1' => 'Value 1',
+                'setting2' => 200,
+                'setting3' => true,
+                'setting6' => ['key' => 'value', 'doge' => ['wow' => '']],
+            ],
+            'group1' => ['baz' => 'bas', 'foo' => 'bar', 'nest' => ['key' => 'value']],
+            'group2' => ['abc' => 12345, 'deep' => ['id' => 45]],
         ];
         $this->assertEquals($expected, $actual);
     }
