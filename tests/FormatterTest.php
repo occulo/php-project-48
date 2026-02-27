2011d03acafe93edfabbf7d940edc0e26eeda44d
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-02-27 04:20:41 +0400

    test(formatter): add tests for plain formatter

--- tests/FormatterTest.php
+++ tests/FormatterTest.php
@@ -21,4 +21,17 @@ class FormatterTest extends TestCase
         $expected = file_get_contents(__DIR__ . '/fixtures/expected_stylish.txt');
         $this->assertSame(trim($expected), trim($actual));
     }
+
+    public function testPlainFormatting(): void
+    {
+        $firstPath = __DIR__ . '/fixtures/json/file1.json';
+        $secondPath = __DIR__ . '/fixtures/json/file2.json';
+        $firstFile = ParserFactory::build($firstPath)->parse($firstPath);
+        $secondFile = ParserFactory::build($secondPath)->parse($secondPath);
+        $differ = new Differ();
+        $diff = $differ->genDiff($firstFile, $secondFile);
+        $actual = FormatterFactory::build('plain')->format($diff);
+        $expected = file_get_contents(__DIR__ . '/fixtures/expected_plain.txt');
+        $this->assertSame(trim($expected), trim($actual));
+    }
 }
