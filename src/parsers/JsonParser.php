064bf1bf7ac9c71d0ecc288718f026489695c8de
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-03-03 01:28:05 +0400

    feat(parser): add basic exception handling

--- src/parsers/JsonParser.php
+++ src/parsers/JsonParser.php
@@ -9,6 +9,13 @@ class JsonParser implements ParserInterface
     public function parse(string $path): array
     {
         $content = file_get_contents($path);
-        return json_decode($content, true);
+        if ($content === false) {
+            throw new \Exception('Failed to read file: ' . $path);
+        }
+        try {
+            return json_decode($content, true, 512, JSON_THROW_ON_ERROR);
+        } catch (\JsonException $e) {
+            throw new \Exception('Failed to decode JSON data: ' . $e->getMessage(), 0, $e);
+        }
     }
 }
--- src/parsers/YamlParser.php
+++ src/parsers/YamlParser.php
@@ -3,6 +3,7 @@
 namespace Hexlet\Code\parsers;
 
 use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Exception\ParseException;
 use Hexlet\Code\ParserInterface;
 
 class YamlParser implements ParserInterface
@@ -10,6 +11,13 @@ class YamlParser implements ParserInterface
     public function parse(string $path): array
     {
         $content = file_get_contents($path);
-        return Yaml::parse($content);
+        if ($content === false) {
+            throw new \Exception('Failed to read file: ' . $path);
+        }
+        try {
+            return Yaml::parse($content);
+        } catch (ParseException $e) {
+            throw new \Exception('Failed to decode YAML data: ' . $e->getMessage(), 0, $e);
+        }
     }
 }
