aaef7c3dfc06b17a9f25553850b3a080487ee497
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-03-05 01:12:37 +0400

    test(parser): load expected values from fixtures

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -26,16 +26,7 @@ class ParserTest extends TestCase
     public function testParsing($filePath): void
     {
         $actual = ParserFactory::build($filePath)->parse($filePath);
-        $expected = [
-            'common' => [
-                'setting1' => 'Value 1',
-                'setting2' => 200,
-                'setting3' => true,
-                'setting6' => ['key' => 'value', 'doge' => ['wow' => '']],
-            ],
-            'group1' => ['baz' => 'bas', 'foo' => 'bar', 'nest' => ['key' => 'value']],
-            'group2' => ['abc' => 12345, 'deep' => ['id' => 45]],
-        ];
+        $expected = require __DIR__ . '/fixtures/expected_parser.php';
         $this->assertEquals($expected, $actual);
     }
 }
