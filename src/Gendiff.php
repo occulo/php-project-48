9bdf34d909086d63afa7356d3406d4c2e16c6bec
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-03-11 19:36:27 +0400

    refactor(gendiff): restructure api and remove obsolete helper

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Hexlet\Code;
+namespace Occulo\Gendiff;
 
-use Hexlet\Code\Parser;
-use Hexlet\Code\Differ;
-use Hexlet\Code\FormatterFactory;
+use Occulo\Gendiff\Parser;
+use Occulo\Gendiff\Differ;
+use Occulo\Gendiff\Formatter\FormatterFactory;
 
-class Application
+class Gendiff
 {
     private Parser $parser;
     private Differ $differ;
@@ -19,7 +19,7 @@ class Application
         $this->factory = new FormatterFactory();
     }
 
-    public function run(string $firstPath, string $secondPath, string $format = 'stylish'): string
+    public function build(string $firstPath, string $secondPath, string $format = 'stylish'): string
     {
         $first = $this->parser->parse($firstPath);
         $second = $this->parser->parse($secondPath);
